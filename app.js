29dc38f22713707aa8a576d837566cc4c0a69225
Mikhail Ivanov <55839915+midevelop@users.noreply.github.com>
2019-11-25 20:11:27 +0300

    feat(web): project

---
+++ app.js
@@ -0,0 +1,12 @@
+// подключение express
+const express = require("express");
+// создаем объект приложения
+const app = express();
+// определяем обработчик для маршрута "/"
+app.get("/", function(request, response){
+
+    // отправляем ответ
+    response.send("<h2>Привет Express!</h2>");
+});
+// начинаем прослушивать подключения на 3000 порту
+app.listen(3000);
