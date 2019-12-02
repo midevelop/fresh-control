5886b3b9ba11d244252ed2584fa3b6cd5a4f8954
Mikhail Ivanov <55839915+midevelop@users.noreply.github.com>
2019-12-02 20:22:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const categoryScheme = require('./model/category')
 
 const Category = mongoose.model("Category", categoryScheme)
-mongoose.connect("mongodb://localhost:27017/fresh-control", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
 
 // создаем объект приложения
 const app = express();
